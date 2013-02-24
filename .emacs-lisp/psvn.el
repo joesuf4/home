3629b25dc40f105a80ad4b8ded22ee592cf2d5ac
Joe Schaefer <joesuf4@gmail.com>
2013-02-23 21:19:49 -0500

    hack to set base-dir to start-dir in svn-status-base-dir

--- .emacs-lisp/psvn.el
+++ .emacs-lisp/psvn.el
@@ -6032,7 +6032,7 @@ Return nil, if not in a svn working copy."
     ;;(message "svn-status-base-dir: %S %S" start-dir base-dir)
     (if (not (eq base-dir 'not-found))
         base-dir
-      ;; (message "calculating base-dir for %s" start-dir)
+       ;;(message "calculating base-dir for %s" start-dir)
       (unless svn-client-version
         (svn-status-version))
       (let* ((base-dir start-dir)
@@ -6040,21 +6040,26 @@ Return nil, if not in a svn working copy."
              (dot-svn-dir (concat base-dir (svn-wc-adm-dir-name)))
              (in-tree (and repository-root (file-exists-p dot-svn-dir)))
              (dir-below (expand-file-name base-dir)))
-        ;; (message "repository-root: %s start-dir: %s" repository-root start-dir)
-        (if (and (<= (car svn-client-version) 1) (< (cadr svn-client-version) 3))
-            (setq base-dir (svn-status-base-dir-for-ancient-svn-client start-dir)) ;; svn version < 1.3
-          (while (when (and dir-below (file-exists-p dot-svn-dir))
-                   (setq base-dir (file-name-directory dot-svn-dir))
-                   (string-match "\\(.+/\\).+/" dir-below)
-                   (setq dir-below
-                         (and (string-match "\\(.*/\\)[^/]+/" dir-below)
-                              (match-string 1 dir-below)))
-                   ;; (message "base-dir: %s, dir-below: %s, dot-svn-dir: %s in-tree: %s" base-dir dir-below dot-svn-dir in-tree)
-                   (when dir-below
-                     (if (string= (svn-status-repo-for-path dir-below) repository-root)
-                         (setq dot-svn-dir (concat dir-below (svn-wc-adm-dir-name)))
-                       (setq dir-below nil)))))
-          (setq base-dir (and in-tree base-dir)))
+        ;;(message "repository-root: %s start-dir: %s" repository-root start-dir)
+        (if (and (>= (car svn-client-version) 1) (> (cadr svn-client-version)
+6))
+            (setq base-dir start-dir)
+
+
+            (if (and (<= (car svn-client-version) 1) (< (cadr svn-client-version) 3))
+                (setq base-dir (svn-status-base-dir-for-ancient-svn-client start-dir)) ;; svn version < 1.3
+              (while (when (and dir-below (file-exists-p dot-svn-dir))
+                       (setq base-dir (file-name-directory dot-svn-dir))
+                       (string-match "\\(.+/\\).+/" dir-below)
+                       (setq dir-below
+                             (and (string-match "\\(.*/\\)[^/]+/" dir-below)
+                                  (match-string 1 dir-below)))
+                       ;;(message "base-dir: %s, dir-below: %s, dot-svn-dir: %s in-tree: %s" base-dir dir-below dot-svn-dir in-tree)
+                       (when dir-below
+                        (if (string= (svn-status-repo-for-path dir-below) repository-root)
+                             (setq dot-svn-dir (concat dir-below (svn-wc-adm-dir-name)))
+                           (setq dir-below nil)))))
+          (setq base-dir (and in-tree base-dir))))
         (svn-puthash start-dir base-dir svn-status-base-dir-cache)
         (svn-status-message 7 "svn-status-base-dir %s => %s" start-dir base-dir)
         base-dir))))
