da6423e803ac736c8c3a1769ddc80c805669e37f
Joe Schaefer <joe@sunstarsys.com>
2021-10-04 17:25:31 -0400

    jinjalint_for_template_linting

--- linter.sh
+++ linter.sh
@@ -115,6 +115,6 @@ else
   # using (installed ".git/hooks/pre-commit" suffix) path,
   # thus "git diff" pipeline inputs
   git diff --name-only "${@:---cached}"
-fi | grep -v /templates/ | grep -Pe "$PCRE_PAT" |
+fi | if [[ "${LINT_TEMPLATES:-}" == yes ]]; then cat; else grep -v /templates/; fi | grep -Pe "$PCRE_PAT" |
   while read -r line; do [[ -f "$line" ]] && echo "$line"; done |
   eval "xargs -rd'\n' -P${XARGS_WORKERS:-$(nproc)} -n${XARGS_MAX_FILES:-64} $LINTER"
