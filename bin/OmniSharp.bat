89b47b94064af534e21ca123647724cd39dfc74d
Joe Schaefer <joseph.schaefer@blackstone.com>
2021-12-12 10:04:42 -0500

    native-linux-now

--- bin/OmniSharp.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell wsl /mnt/c/Users/schaefj/src/omnisharp-roslyn/artifacts/publish/OmniSharp.Stdio.Driver/mono/OmniSharp.exe %*
--- bin/gopls.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell wsl zsh "-c" "'. ~/.zshenv && exec gopls $@'" -- %*
