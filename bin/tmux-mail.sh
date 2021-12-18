1c01c08c7845e6ac31c8223805f495746c546fc8
Joe Schaefer <joseph.schaefer@blackstone.com>
2021-12-18 12:19:24 -0500

    mail

--- bin/tmux-mail.sh
+++ bin/tmux-mail.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+messages="$(grep -c "^From " /var/spool/mail/$USER 2>/dev/null)"
 
-[[ "$(stat -c %s /var/spool/mail/$USER)" -gt 600 ]] && echo "📧"
+if [[ "$messages" -gt 1 ]]; then
+  ((--messages))
+  echo "Mail($messages)"
+fi
