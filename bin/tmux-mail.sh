4278ae342d4826a828c3cc2b48fe8736d2cae201
Joe Schaefer <joe@sunstarsys.com>
2021-12-19 11:24:00 -0500

    tmux-mail.sh

--- bin/tmux-mail.sh
+++ bin/tmux-mail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 messages="$(grep -c "^From " /var/spool/mail/$USER 2>/dev/null)"
-messages_read="$(grep -c "^Status: R" /var/spool/mail/$USER 2>/dev/null)"
+messages_read="$(perl -l -000 -ne 'BEGIN{$count=0} $count++ if m/^From / and /^Status: RO/m; END {print $count}' /var/spool/mail/$USER 2>/dev/null)"
 
 if [[ "$messages" -gt "$messages_read" ]]; then
   echo "Mail($((messages - messages_read)))"
