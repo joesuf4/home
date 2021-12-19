3e35c9d29dae7556ca0d1a61f0ff529f71db098f
Joe Schaefer <joe@sunstarsys.com>
2021-12-19 09:37:23 -0500

    messages_read

--- bin/tmux-mail.sh
+++ bin/tmux-mail.sh
@@ -3,6 +3,5 @@ messages="$(grep -c "^From " /var/spool/mail/$USER 2>/dev/null)"
 messages_read="$(grep -c "^Status: R" /var/spool/mail/$USER 2>/dev/null)"
 
 if [[ "$messages" -gt "$messages_read" ]]; then
-  messages=$((messages - messages_read))
-  echo "Mail($messages)"
+  echo "Mail($((messages - messages_read)))"
 fi
