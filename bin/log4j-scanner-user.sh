196e2cdeef52a3915a4a1f84188d83ff77069864
Joe Schaefer <joe@sunstarsys.com>
2021-12-30 14:16:38 -0500

    linting

--- bin/log4j-scanner-user.sh
+++ bin/log4j-scanner-user.sh
@@ -9,7 +9,8 @@ if ! [[ -f $PUB_KEY ]]; then
   exit 1
 fi
 
-TMP="$(cat <<'EOT'
+TMP="$(
+  cat <<'EOT'
 _eks_node_report_cmds=(adduser addkey)
 alias _eks_report_node_adduser='eks node-batch . "getent passwd $SCAN_USER || sudo useradd -G adm -m -d /home/$SCAN_USER -s /bin/bash $SCAN_USER"'
 alias _eks_report_node_addkey='eks node-batch . "sudo -u $SCAN_USER mkdir -p ~$SCAN_USER/.ssh && sudo -u $SCAN_USER bash -c \"cat > ~$SCAN_USER/.ssh/authorized_keys\"" <$PUB_KEY'
@@ -20,5 +21,5 @@ for sig in EXIT TERM INT HUP; do
   trap 'cp ~/src/bcscli/eksrc ~/.eksrc' $sig
 done
 
-echo "$TMP" >> ~/.eksrc
+echo "$TMP" >>~/.eksrc
 eks report global nodes
