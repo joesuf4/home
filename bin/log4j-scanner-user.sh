42edccc0d55e869d12ea2ba66bf1265cf1ddccb2
Joe Schaefer <joe@sunstarsys.com>
2021-12-30 13:51:53 -0500

    usage

--- bin/log4j-scanner-user.sh
+++ bin/log4j-scanner-user.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/zsh -i
+# USAGE: $0 [$1]
+# $1 - optional path to public key file (defaults to $HOME/tmp/$SCAN_USER.pubkey)
+
 export SCAN_USER=gd-nessus-p-app
 export PUB_KEY="${1-$HOME/tmp/$SCAN_USER.pubkey}"
 if ! [[ -f $PUB_KEY ]]; then
@@ -12,8 +15,10 @@ alias _eks_report_node_adduser='eks node-batch . "getent passwd $SCAN_USER || su
 alias _eks_report_node_addkey='eks node-batch . "sudo -u $SCAN_USER mkdir -p ~$SCAN_USER/.ssh && sudo -u $SCAN_USER bash -c \"cat > ~$SCAN_USER/.ssh/authorized_keys\"" <$PUB_KEY'
 EOT
 )"
+
 for sig in EXIT TERM INT HUP; do
   trap 'cp ~/src/bcscli/eksrc ~/.eksrc' $sig
 done
+
 echo "$TMP" >> ~/.eksrc
 eks report global nodes
