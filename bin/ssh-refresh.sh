ea27857da1712657b071c44e80471f74a5dd0865
Joe Schaefer <joe@sunstarsys.com>
2022-09-16 18:53:29 -0400

    mine

---
+++ bin/ssh-refresh.sh
@@ -0,0 +1,16 @@
+#!/usr/local/bin/zsh
+
+. ~joe/.zshenv
+
+slice=1
+
+for region ad in ${(kv)OCI_AD}
+do
+    for id in {1..$ad}
+    do
+        [[ -z "$slice" || $slice -eq $id ]] || continue
+        ssh $OCI_HOST_PREFIX-$id.$region echo $region ok
+    done
+done
+
+ssh 127.0.0.1 true
---
+++ bin/zfs-backups.sh
@@ -0,0 +1,20 @@
+#!/usr/local/bin/zsh
+PATH=/usr/local/bin:/sbin:/usr/bin
+
+ZULU=$(date -Iseconds | tr '+' 'Z')
+
+for svn_repo in /x1/repos/svn/*
+do
+    LAST=$(ls -t $svn_repo/.zfs/snapshot | head -n 1)
+    if [ -z "$LAST" ]
+    then
+        LAST=baseline
+        zfs snapshot tank$svn_repo@$LAST
+        TMPFILE=/x1/backups/svn/$(basename $svn_repo)-$LAST.zfs
+        zfs send -rc tank$svn_repo@$LAST > $TMPFILE
+        continue
+    fi
+    TMPFILE=/x1/backups/svn/$(basename $svn_repo)-$ZULU.zfs
+    zfs snapshot tank$svn_repo@$ZULU
+    zfs send -D -Rci $LAST tank$svn_repo@$ZULU > $TMPFILE
+done
