b2e5703c143d7c0e7d3a10494e7e4e139c7ca8ae
Joe Schaefer <joe@sunstarsys.com>
2022-12-05 20:35:35 -0500

    Update generate-dbm-groups.sh

--- bin/generate-dbm-groups.sh
+++ bin/generate-dbm-groups.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/zsh
 declare -A group
 
-for line in $(dbmmanage $1 view); do
+for g in $(grep '^ *@' $1 | cut -d= -f1 | awk '{print $1}'); do
+  : "${group[${g#@}]:=}"
+done
+
+for line in $(dbmmanage $2 view); do
   user=$(echo "$line" | cut -d: -f1 | tr -d '\n')
   groupline=$(echo "$line" | cut -d: -f3 | tr -d '\n')
   for g in $(echo "$groupline" | tr ',' ' ' | tr -d '\n'); do
