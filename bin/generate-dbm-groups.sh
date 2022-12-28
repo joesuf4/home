730f66b25d69053c7abf802261cb3eb472b7709e
Joe Schaefer <joe@sunstarsys.com>
2022-12-28 14:25:28 -0500

    set-orion:watchers

--- bin/generate-dbm-groups.sh
+++ bin/generate-dbm-groups.sh
@@ -5,10 +5,10 @@ for g in $(grep '^ *@' $1 | cut -d= -f1 | awk '{print $1}'); do
   : "${group[${g#@}]:=}"
 done
 
-for line in $(dbmmanage $2 view); do
-  user=$(echo "$line" | cut -d: -f1 | tr -d '\n')
-  groupline=$(echo "$line" | cut -d: -f3 | tr -d '\n')
-  for g in $(echo "$groupline" | tr ',' ' ' | tr -d '\n'); do
+for line in $(dbmmanage $2 view | cut -d: -f1-3); do
+  user="$(echo "$line" | cut -d: -f1)"
+  groupline="$(echo "$line" | cut -d: -f3)"
+  for g in $(echo "$groupline" | tr ',' '\n'); do
     : "${group[$g]:=}"
     group[$g]+="$user,"
   done
