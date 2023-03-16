0ac6647ef41faef1fc56282648d0bd7abdef20fe
Joseph Schaefer <joseph.schaefer@citrix.com>
2023-03-16 19:15:15 -0400

    mbox-emoji

--- bin/tmux-mail.sh
+++ bin/tmux-mail.sh
@@ -3,5 +3,5 @@ messages="$(grep -c "^From " /var/spool/mail/$USER 2>/dev/null)"
 messages_read="$(perl -l -000 -ne 'BEGIN{$count=0} $count++ if m/^From / and /^Status: RO/m; END {print $count}' /var/spool/mail/$USER 2>/dev/null)"
 
 if [[ "$messages" -gt "$messages_read" ]]; then
-  echo "Mail($((messages - messages_read)))"
+  echo "📬($((messages - messages_read)))"
 fi
