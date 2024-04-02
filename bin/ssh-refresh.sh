edec977f10abbf5ddca302817bfb4d6951fc6789
Joe Schaefer <joe@sunstarsys.com>
2024-04-01 20:27:37 -0400

    no-slices

--- bin/ssh-refresh.sh
+++ bin/ssh-refresh.sh
@@ -1,14 +1,14 @@
-#!/usr/local/bin/zsh -l
-
-slice=1
+#!/usr/local/bin/zsh -i
 
 for region ad in ${(kv)OCI_AD}
 do
     for id in {1..$ad}
     do
         [[ -z "$slice" || $slice -eq $id ]] || continue
-        ssh $OCI_HOST_PREFIX-$id.$region echo $region ok
+	ssh $OCI_HOST_PREFIX-$id.$region echo $region reconnected
     done
 done
 
 ssh 127.0.0.1 true
+wait
+
