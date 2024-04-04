bda077a52e1cce1c1da032bc3fbc7ca369b3f5b0
Joe Schaefer <joe@sunstarsys.com>
2024-04-04 15:11:21 -0400

    ocirc-reflow

--- bin/ssh-refresh.sh
+++ bin/ssh-refresh.sh
@@ -9,7 +9,7 @@ do
     for id in {1..$ad}
     do
         [[ -z "$slice" || $slice -eq $id ]] || continue
-	timeout 30 pty -nd pty-driver.pl -- zsh -ic "ssh $OCI_HOST_PREFIX-$id.$region zsh -ic \"sleep 6; ptyd timeout 10 sudo echo \\\$(hostname).$region connected.\"" &
+	timeout 10 pty -nd pty-driver.pl -- zsh -ic "ssh -t $OCI_HOST_PREFIX-$id.$region sudo echo \$(hostname).$region connected." &
     done
 done
 
