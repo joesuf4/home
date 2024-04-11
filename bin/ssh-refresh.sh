af49f803d13aec6cfc2018353d950a08753819a4
Joe Schaefer <joe@sunstarsys.com>
2024-04-11 11:51:44 -0400

    tunes

--- bin/ssh-refresh.sh
+++ bin/ssh-refresh.sh
@@ -9,7 +9,7 @@ do
     for id in {1..$ad}
     do
         [[ -z "$slice" || $slice -eq $id ]] || continue
-	timeout 20 pty -nd pty-driver.pl ssh $OCI_HOST_PREFIX-$id.$region true &
+	timeout 30 pty -nd pty-driver.pl ssh $OCI_HOST_PREFIX-$id.$region true &
     done
 done
 
