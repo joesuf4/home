c970f7baec4295571f8ac35a8bf9202edacf654a
Joe Schaefer <joe@sunstarsys.com>
2024-04-16 11:23:22 -0400

    s/pty-agent/ssh-agent/g

--- bin/ssh-refresh.sh
+++ bin/ssh-refresh.sh
@@ -3,15 +3,17 @@
 . ~/.zshenv
 
 slice=
+export SSH_AGENT_PID="$(pgrep -u $USER -f ssh-agent)"
+export SSH_AUTH_SOCK="$(command ls -t /tmp/ssh-$USER/agent.* | head -n 1)"
 
 for region ad in ${(kv)OCI_AD}
 do
     for id in {1..$ad}
     do
         [[ -z "$slice" || $slice -eq $id ]] || continue
-	timeout 30 pty -nd pty-driver.pl ssh $OCI_HOST_PREFIX-$id.$region true &
+	timeout 30 ssh $OCI_HOST_PREFIX-$id.$region true &
     done
 done
 
-pty -nd pty-driver.pl -- ssh 127.0.0.1 true
+ssh 127.0.0.1 true
 wait
