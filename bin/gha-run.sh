97274e82eb7e7ea802984ffe9270cdcd70aa5be1
Joe Schaefer <jschaefer@cleardemand.com>
2025-01-12 21:21:49 -0500

    gha-runner

--- bin/gha-run.sh
+++ bin/gha-run.sh
@@ -1,3 +1,7 @@
-#!/usr/bin/zsh
+#!/usr/bin/zsh -i
 [[ "$#" > 0 ]] && ./config.sh "$@"
-exec docker run -d -t -v "$PWD":/src -v --rm schaefj/gha-runner:latest
+WD="$(basename "$PWD")"
+rm -rf _work/"$WD"
+mkdir -p _work/"$WD"
+sudo mount --onlyonce -t tmpfs -o size=1G,mode=1777 none _work/"$WD"
+exec docker run -d --rm -t -v "$PWD":/src -v "$PWD/_work/$WD":"/src/_work/$WD" schaefj/gha-runner:latest
