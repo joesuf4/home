aef28357b3f0e212c738079e1dbd0c30ce1e4b35
Joe Schaefer <jschaefer@cleardemand.com>
2025-01-13 10:31:35 -0500

    gha-runner

--- bin/gha-run.sh
+++ bin/gha-run.sh
@@ -3,5 +3,5 @@
 WD="$(basename "$PWD")"
 rm -rf _work/"$WD"
 mkdir -p _work/"$WD"
-sudo mount --onlyonce -t tmpfs -o size=1G,mode=1777 none _work/"$WD"
+sudo mount --onlyonce -t tmpfs -o size=100M,mode=1777 none _work/"$WD"
 exec docker run -d --rm -t -v "$PWD":/src -v "$PWD/_work/$WD":"/src/_work/$WD" schaefj/gha-runner:latest
