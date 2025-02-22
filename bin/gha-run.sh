631f13509c30100ffa148ce4cd6c2f8e8fad6219
Joe Schaefer <jschaefer@cleardemand.com>
2025-02-22 13:24:16 -0500

    updates

--- bin/gha-run.sh
+++ bin/gha-run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/zsh -i
-[[ "$#" > 0 ]] && ./config.sh "$@"
+[[ "$#" -gt 0 ]] && ./config.sh "$@"
 WD="$(basename "$PWD")"
+#docker kill "gha_runner_$WD"
+#sleep 3
 rm -rf _work/"$WD"
 mkdir -p _work/"$WD"
-sudo mount --onlyonce -t tmpfs -o size=512M,mode=1777,noatime,nosuid,nodev none _work/"$WD"
+sudo mount --onlyonce -t tmpfs -o size=2G,mode=1777,noatime,nosuid,nodev none _work/"$WD"
 exec docker run -d --security-opt no-new-privileges --name "gha_runner_$WD" --rm -t -v "$PWD":/src -v "$PWD/_work/$WD":"/src/_work/$WD" schaefj/gha-runner:latest
