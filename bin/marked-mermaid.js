d71f9d10958c59c8aec43a24e2081c2c986f8664
Joe Schaefer <jschaefer@cleardemand.com>
2025-03-21 16:48:53 -0400

    sleep-half-second

--- bin/marked-mermaid.js
+++ bin/marked-mermaid.js
@@ -27,37 +27,38 @@ process.stdin.on('end', () => {
       packages: ['base', 'newcommand', 'action']
     }
   };
-  </script>
-  <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/startup.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/mermaid@10.7.0/dist/mermaid.min.js"></script>
-  <script src="https://www.sunstarsys.com/editor.md/lib/codemirror/codemirror.min.js"></script>
-  <script src="https://www.sunstarsys.com/editor.md/lib/codemirror/addons.min.js"></script>
-  <script src="https://www.sunstarsys.com/editor.md/lib/codemirror/modes.min.js"></script>
-  <script blocking="render" type="text/javascript" async>
-    var idx = 0;
-    async function render () {
-      for (const e of $("body").find(".mermaid").toArray()) {
-        const {svg} = await mermaid.render("mermaid-" + ++idx, $(e).text());
-        e.outerHTML = "<center>" + svg + "</center>";
-      }
-
-      colorize = function(collection, defaultMode) {
-        if (!collection) collection = document.body.getElementsByTagName("code");
-        for (var i = 0; i < collection.length; ++i) {
-          var node = collection[i];
-          var mode = node.className.replace("language-","");
-          if (mode == "c") mode = "text/x-csrc";
-          if ($(node).hasClass("cm-s-solarized") || !mode) continue;
-          var text = $(node).text();
-          node.innerHTML = ""
-          CodeMirror.runMode(text, mode, node);
-          node.className += " cm-s-solarized";
-        }
-      }
-      colorize();
+</script>
+<script src="https://www.sunstarsys.com/editor.md/lib/codemirror/codemirror.min.js"></script>
+<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/startup.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/mermaid@10.7.0/dist/mermaid.min.js"></script>
+<script src="https://www.sunstarsys.com/editor.md/lib/codemirror/addons.min.js"></script>
+<script src="https://www.sunstarsys.com/editor.md/lib/codemirror/modes.min.js"></script>
+<script blocking="render" type="text/javascript" async>
+  var idx = 0;
+  async function render () {
+    for (const e of $("body").find(".mermaid").toArray()) {
+      const {svg} = await mermaid.render("mermaid-" + ++idx, $(e).text());
+      e.outerHTML = "<center>" + svg + "</center>";
     }
-    render();
-  </script>
+
+    colorize = function(collection, defaultMode) {
+      if (!collection) collection = document.body.getElementsByTagName("code");
+      for (var i = 0; i < collection.length; ++i) {
+        var node = collection[i];
+        var mode = node.className.replace("language-","");
+        if (mode == "c") mode = "text/x-csrc";
+        if ($(node).hasClass("cm-s-solarized") || !mode) continue;
+        var text = $(node).text();
+        node.innerHTML = ""
+        CodeMirror.runMode(text, mode, node);
+        node.className += " cm-s-solarized";
+      }
+    }
+    await new Promise(r=>setTimeout(r,500));
+    colorize();
+  }
+  render();
+</script>
 `
     data = marked.parse(data,{gfm: true, breaks: true})
     process.stdout.write(data)
