a4f8e8ede139a619196ab4234c57091c9a77e6f7
Joe Schaefer <jschaefer@cleardemand.com>
2025-04-07 10:20:15 -0400

    USAGE

--- bin/stackcollapse-bpftrace.pl
+++ bin/stackcollapse-bpftrace.pl
@@ -2,7 +2,7 @@
 #
 # stackcollapse-bpftrace.pl	collapse bpftrace samples into single lines.
 #
-# USAGE ./stackcollapse-bpftrace.pl infile > outfile
+# USAGE ./stackcollapse-bpftrace.pl [++|-n] [<file> ...] <infile > outfile
 #
 # Example input:
 #
