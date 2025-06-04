36a495b9dfae02228970da71b649d577a2a56684
Joe Schaefer <jschaefer@cleardemand.com>
2025-06-04 17:38:36 -0400

    tweaks

--- bin/tmux-free-c-drive.sh
+++ bin/tmux-free-c-drive.sh
@@ -2,10 +2,10 @@
 
 df -h /mnt/c | (
   read -r _
-  [[ -t 2 ]] && awk '$4 ~ /^[1-4]?[0-9]G/ {printf("/mnt/c☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠ ");}
-   {print $4 "B"}' || awk '$4 ~ /^[0-9]G/ {color="#[fg=red,bright]"}
-   $4 ~ /^[1-4][0-9]G/ {color="#[fg=yellow,bright]";warning="/mnt/c☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠ "}
-   $4 ~ /^[5-9][0-9]G/ {color="#[fg=white,bright]";warning="/mnt/c☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠ "}
-   $4 ~ /^[1-4]?[0-9]G/ {printf("/mnt/c☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠ ")}
+  [[ -t 2 ]] && awk '$4 ~ /^[1-4]?[0-9]G/ {printf("/mnt/c☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠ ");}
+   {print $4 "B"}' || awk '$4 ~ /^[0-9]G/ {color="#[fg=brightred]"}
+   $4 ~ /^[1-4][0-9]G/ {color="#[fg=orange]";warning="/mnt/c☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠ "}
+   $4 ~ /^[5-9][0-9]*G/ {color="#[fg=brightyellow]";warning="/mnt/c☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠ "}
+   $4 ~ /^[1-4]?[0-9]G/ {printf("/mnt/c☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠ ")}
    {printf("%s%s%sB\n", color, warning, $4)} BEGIN{color="";warning=""}'
 )
