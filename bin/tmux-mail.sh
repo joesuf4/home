8bd169aee9a8b4ccb927d2c9118192c3afb05e07
Joe Schaefer <jschaefer@cleardemand.com>
2025-06-03 20:25:39 -0400

    fix-code

--- bin/tmux-mail.sh
+++ bin/tmux-mail.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 messages="$(grep -c "^From " /var/spool/mail/$USER 2>/dev/null)"
 messages_read="$(perl -l -000 -ne 'BEGIN{$count=0} $count++ if m/^From / and /^Status: RO/m; END {print $count}' /var/spool/mail/$USER 2>/dev/null)"
-free_space="$(df -h /mnt/c | (read _; awk '{print $4}'))"
+
 if [[ "$messages" -gt "$messages_read" ]]; then
-  echo "📬($((messages - messages_read))) ${free_space}"
+  echo "📬($((messages - messages_read)))"
 fi
-echo "${free_space}"
