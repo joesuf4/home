a4670e5ed60bdefb47f5a1efeba905bd77d4110b
Joe Schaefer <jschaefer@cleardemand.com>
2025-06-26 15:38:43 -0400

    s/orange/magenta/g

--- bin/tmux-free-swap.sh
+++ bin/tmux-free-swap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 grep SwapFree: /proc/meminfo |
   awk '$2 < 32*(1024**2) {color="#[fg=brightyellow,bg=terminal]"}
-       $2 < 16*(1024**2) {color="#[fg=orange,bg=terminal]"}
+       $2 < 16*(1024**2) {color="#[fg=brightmagenta,bg=terminal]"}
        $2 <  8*(1024**2) {color="#[fg=brightred,bg=terminal]"}
        {printf("%s%dGB\n",color,$2/1024**2)}'
