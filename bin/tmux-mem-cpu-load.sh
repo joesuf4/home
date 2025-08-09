b80d8c92f49897f47b02e57ae3248e79056479ff
Joe Schaefer <jschaefer@cleardemand.com>
2025-08-09 15:56:47 -0400

    fubar

--- bin/tmux-mem-cpu-load.sh
+++ bin/tmux-mem-cpu-load.sh
@@ -5,7 +5,7 @@ rv="$(~/bin/tmux-mem-cpu-load "$@")"
 if [[ "${rv##* }" -ge $(($(nproc)*2)) ]]; then
   c="fg=brightred,bg=terminal"
 elif [[ "${rv##* }" -ge $(($(nproc))) ]]; then
-  c="fg=brightmagenta,bg=terminal"
+  c="fg=magenta,bg=terminal"
 elif [[ "${rv##* }" -ge $(($(nproc)/2)) ]]; then
   c="fg=brightyellow,bg=terminal"
 else
